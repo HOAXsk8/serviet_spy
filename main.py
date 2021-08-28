1406c80ee9c5f712934c23b28c52d1c5e80d5a85
jhowd <jhowden86@outlook.com>
2021-08-28 16:20:54 +0100

    Operational

--- main.py
+++ main.py
@@ -6,10 +6,12 @@ momentarily establish a connection to a listening port and listen for a response
 Use at your own risk! """
 
 import socket
+import time
 import csv
 import database_functions as db
 import psutil
 import multiprocessing
+from queue import Queue
 
 
 def get_ip_range(ip):
@@ -41,6 +43,7 @@ def scanner(ip, port, TIMEOUT=0.2):
         s.settimeout(TIMEOUT)
         connection = s.connect_ex((ip, int(port)))
         if connection == 0:
+            s.close()
             return port
         else:
             pass
@@ -59,7 +62,9 @@ def get_system_usage():
 
 def worker():
     execute = True
+    q = Queue
     port_list = create_port_list()
+    start_time = time.time()
 
     while execute:
         row = db.execute_sql('read', db.SELECT_RANDOM_ROW)
@@ -68,13 +73,21 @@ def worker():
 
         for ip in ip_range:
             for port in port_list:
+                # print(f'Scanning {ip}:{port}')
                 open_port = scanner(ip, port)
                 if open_port:
                     db.execute_sql('write', db.INSERT_SERVICE_DATA.format(ip, open_port))  # Write open ip:port to database.
-            db.execute_sql('write', db.UPDATE_ROW.format(cidr_ip))  # Update the scanned row (scanned_status = true)
+
+        db.execute_sql('write', db.UPDATE_ROW.format(cidr_ip))  # Update the scanned row (scanned_status = true)
+
+        total_time_sec = time.time() - start_time
+        total_time_min = total_time_sec / 60
+        output = "{:.2f}".format(total_time_min)
+        print(output)
 
 
 if __name__ == '__main__':
+    worker()
     spawn_workers = True
     work_force = 0
 
@@ -89,4 +102,4 @@ if __name__ == '__main__':
             spawn_workers = False
             print(f'CPU used: {cpu_usage}%')
             print(f'Memory used: {memory_usage}%')
-            print(f'{work_force} active workers')
+            print(f'Active workers: {work_force}')
